c98b5d6438e1176726e18aef297e4c39375c56c5
k3_kaimu <enjouzensyou.boinc@gmail.com>
2012-08-04 22:22:38 +0900

    arrange a sample

--- _sample0.d
+++ _sample0.d
@@ -3,9 +3,11 @@ import std.stdio;
 
 pragma(lib, "blas");
 pragma(lib, "dblet");
-pragma(lib, "scid");
+pragma(lib, "scid");    //現在はBLAS, LAPACKのbinding libraryとしてscidを使っているが、現在dblet.bindingに独自のbindingライブラリを作成中
 
 void main(){
+
+    //行列積
     auto A = Matrix!double(3, 3),
          B = Matrix!double(3, 3),
          C = Matrix!double(3, 3);
@@ -28,13 +30,23 @@ void main(){
     //演算は遅延評価的に行われ、この場合はコンパイル時簡約化によりblas level 3のgemmが実行される。
     auto ans = alpha * A * B + beta * C;
     /* 今回の場合のコンパイル時簡約化の手順
-     * alpha * A * B + beta * C
-     * scal(alpha, A) * B + scal(beta, C)
-     * gemm(alpha, A, B, 0, Dummy) + scal(beta, C)
-     * gemm(alpha, A, B, beta, C)
+     * 1. alpha * A * B + beta * C
+     * 2. scal(alpha, A) * B + scal(beta, C)
+     * 3. gemm(alpha, A, B, 0, Dummy) + scal(beta, C)
+     * 4. gemm(alpha, A, B, beta, C)
      */
     
+    //ansの型を表示してみる
     writeln(typeof(ans).stringof);  //MatrixExpr!("gemm",double,double,Matrix!(double),Matrix!(double),double,Matrix!(double))
+    
+    //ansをそのまま表示しても遅延評価のため、演算結果は表示されない
+    writeln(ans);   //MatrixExpr!("gemm",double,double,Matrix!(double),Matrix!(double),double,Matrix!(double))(3, 3, 3, Matrix!(double)(3, 3, [1, 2, 9, 8, 10, -5, 3, 8, -1]), Matrix!(double)(3, 3, [9, 3, -8, 8, 11, 6, 3, 2.3, 1]), -2, Matrix!(double)(3, 3, [3, 8, 6, 3, 4, 1, 1.2, 8, -2]))
+    
+    //ansを実際に計算してみる
     writeln(ans.evalExpr);          //Matrix!(double)(3, 3, [21, -64, 210, 336, 514, 31, 70.8, 95, 47.5])
-    //格納形式はcolumnメジャーなので、この順番で解は表示される。
+    //解の格納形式ももちろんcolumnメジャー
+    
+    writeln(ans[0, 0]);             //対角要素だけ計算してみる
+    writeln(ans[1, 1]);             //遅延評価されているので、他の要素は演算されない
+    writeln(ans[2, 2]);             //そのため、全体が必要なければ(部分だけでいいのであれば)高速
 }
